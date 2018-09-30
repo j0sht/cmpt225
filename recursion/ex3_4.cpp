3b7ddbc343c0bb7dd1874615b8b5a03e416aa14f
Joshua Tate <jmtate@icloud.com>
2018-09-30 15:31:36 -0700

    Added implementation of SumTwoDArray algorithm

---
+++ recursion/ex3_4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Input:
+//  - Two dimensional array A of size n*n
+//  - Integers i, j, n, representing number of rows-1, columns-1, and n-1
+// Output:
+//  - Sum of integers in two dimensional array
+int sumTwoDArray(int A[][3], int i, int j, int n) {
+    if (i == 0 && j == 0) return A[i][j];
+    else if (j == 0) return A[i][j] + sumTwoDArray(A,i-1,n,n);
+    else return A[i][j] + sumTwoDArray(A,i,j-1,n);
+}
+
+int main() {
+    int A[3][3] = {{1,2,3},
+		   {4,5,6},
+		   {7,8,9}};
+    cout << "Sum of 1+2+..+9 = " << sumTwoDArray(A,2,2,2) << endl;
+    return 0;
+}
