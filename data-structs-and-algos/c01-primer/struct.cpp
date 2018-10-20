5081f876cf0eb5a42f166a50bf6c0dd093eef5ea
Joshua Tate <jmtate@icloud.com>
2018-10-19 18:31:41 -0700

    Added function to print Passenger struct

--- data-structs-and-algos/c01-primer/struct.cpp
+++ data-structs-and-algos/c01-primer/struct.cpp
@@ -13,10 +13,34 @@ struct Passenger {
     bool isFreqFlyer;
     string freqFlyerNo;
 };
+typedef Passenger* PassengerPtr;
+
+void print(const Passenger& pass);
 
 int main() {
     // Passenger allocated in main stack space
     Passenger pass = { "John Smith", VEGETARIAN, true, "123456" };
+    print(pass);
+    // Dynamically allocated struct (stored in heap memory)
+    // The 'new' operator dynamically allocates the correct amount of storage
+    //  for an object of a given type and returns a pointer to this object.
+    //  i.e. The value of the pointer is the address where the object lives
+    //       in memory.
+    PassengerPtr p = new Passenger;
+    // Two ways to access p's member variables
+    //  i) The ugly way (*p).name
+    (*p).name = "Josh";
+    // ii) Pretty way p->name
+    p->mealPref = LOW_FAT;
+    p->isFreqFlyer = false;
+    // Need to derefrence to conform to function's argument type
+    print(*p);
+    // Remember to delete any allocated memory
+    delete p;
+    return 0;
+}
+
+void print(const Passenger& pass) {
     cout << "Name: " << pass.name << endl;
     cout << "Meal preference: ";
     switch (pass.mealPref) {
@@ -40,44 +64,4 @@ int main() {
     } else {
 	cout << "No\n";
     }
-
-    // Dynamically allocated struct (stored in heap memory)
-    // The 'new' operator dynamically allocated the correct amount of storage
-    //  for an object of a given type and returns a pointer to this object.
-    //  i.e. The value of the pointer is the address where the object lives
-    //       in memory.
-    Passenger* p = new Passenger;
-    // Two ways to access p's member variables
-    //  i) The ugly way (*p).name
-    // ii) Pretty way p->name
-    (*p).name = "Josh";
-    p->mealPref = LOW_FAT;
-    p->isFreqFlyer = false;
-    cout << "Name: " << p->name << endl;
-    cout << "Meal preference: ";
-    switch (p->mealPref) {
-    case 0: // Alternatively, NO_PREF
-	cout << "No preference\n";
-	break;
-    case 1:
-	cout << "Regular\n";
-	break;
-    case 2:
-	cout << "Low fat\n";
-	break;
-    case 3:
-	cout << "Vegetarian\n";
-    }
-    cout << "Is frequent flyer? ";
-    if (p->isFreqFlyer) {
-	cout << "Yes\n";
-	cout << "Frequent flyer number: "
-	     << p->freqFlyerNo << endl;
-    } else {
-	cout << "No\n";
-    }
-
-    // Remember to delete any allocated memory
-    delete p;
-    return 0;
 }
