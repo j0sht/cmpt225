9a2eb071fd004e06408f41ba684a9c1edee1e9dc
Joshua Tate <jmtate@icloud.com>
2018-10-19 20:29:13 -0700

    Tried to clean up LinkedStack a bit

--- assignment2/LinkedStack.cpp
+++ assignment2/LinkedStack.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : LinkedStack.cpp
-// Author      : 
-// Version     :
+// Author      : Joshua Tate
+// Version     : 1
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
@@ -13,8 +13,6 @@
 using std::string;
 using namespace std;
 
-
-
 class StringNode {				        // a node in a list of strings
 private:
   string elem;				                // element value
@@ -25,83 +23,79 @@ private:
 
 class StringLinkedList {			        // a linked list of strings
 public:
-  StringLinkedList();				        // empty list constructor
-  ~StringLinkedList();			                // destructor
-  bool empty() const;				        // is list empty?
-  const string& front() const;		                // get front element
-  void addFront(const string& e);		        // add to front of list
-  void removeFront();				        // remove front item list
+    StringLinkedList();				        // empty list constructor
+    ~StringLinkedList();			        // destructor
+    bool empty() const;				        // is list empty?
+    const string& front() const;		        // get front element
+    void addFront(const string& e);		        // add to front of list
+    void removeFront();				        // remove front item list
 private:
-  StringNode* head;				        // pointer to the head of list
+    StringNode* head;				        // pointer to the head of list
 };
 
-StringLinkedList::StringLinkedList()			// constructor
-  : head(NULL) { }
+StringLinkedList::StringLinkedList() : head(NULL) {}	// constructor
 
-StringLinkedList::~StringLinkedList()			// destructor
-  { while (!empty()) removeFront(); }
+StringLinkedList::~StringLinkedList() {			// destructor
+    while (!empty())
+	removeFront();
+}
 
-bool StringLinkedList::empty() const			// is list empty?
-  { return head == NULL; }
+bool StringLinkedList::empty() const {			// is list empty?
+    return head == NULL;
+}
 
-const string& StringLinkedList::front() const		// get front element
-  { return head->elem; }
-
-
+const string& StringLinkedList::front() const {		// get front elements
+    return head->elem;
+}
 
 void StringLinkedList::addFront(const string& e) {	// add to front of list
-  StringNode* v = new StringNode;			// create new node
-  v->elem = e;					        // store data
-  v->next = head;					// head now follows v
-  head = v;						// v is now the head
+    StringNode* v = new StringNode;			// create new node
+    v->elem = e;					// store data
+    v->next = head;					// head now follows v
+    head = v;						// v is now the head
 }
-
-
 
 void StringLinkedList::removeFront() {		        // remove front item
-  StringNode* old = head;				// save current head
-  head = old->next;					// skip over old head
-  delete old;						// delete the old head
+    StringNode* old = head;				// save current head
+    head = old->next;					// skip over old head
+    delete old;						// delete the old head
 }
 
+typedef string Elem;				        // stack element type
+class LinkedStack {				        // stack as a linked list
+public:
+    LinkedStack();				        // constructor
+    int size() const;				        // number of items in the stack
+    bool empty() const;			                // is the stack empty?
+    const Elem& top() const;	                        // the top element
+    void push(const Elem& e);  		                // push element onto stack
+    void pop();		                                // pop the stack
+private:                                	        // member data
+    StringLinkedList S;			                // linked list of elements
+    int n;					        // number of elements
+};
 
-  typedef string Elem;				        // stack element type
-    class LinkedStack {				        // stack as a linked list
-    public:
-      LinkedStack();				        // constructor
-      int size() const;				        // number of items in the stack
-      bool empty() const;			        // is the stack empty?
-      const Elem& top() const;	                        // the top element
-      void push(const Elem& e);  		        // push element onto stack
-      void pop();		                        // pop the stack
-    private:                                	        // member data
-      StringLinkedList S;			        // linked list of elements
-      int n;					        // number of elements
-    };
+LinkedStack::LinkedStack() : S(), n(0) {}               // constructor
 
-LinkedStack::LinkedStack()
-    : S(), n(0) { }				        // constructor
+bool LinkedStack::empty() const {
+    return n == 0;                                      // is the stack empty?
+}
 
-  bool LinkedStack::empty() const
-    { return n == 0; }				        // is the stack empty?
+const Elem& LinkedStack::top() const {
+    return S.front();
+}
 
-
-
-  const Elem& LinkedStack::top() const {
-      return S.front();
-  }
-
-  void LinkedStack::push(const Elem& e) {	        // push element onto stack
+void LinkedStack::push(const Elem& e) {	                // push element onto stack
     ++n;
     S.addFront(e);
-  }
+}
 						        // pop the stack
-  void LinkedStack::pop() {
-	  --n;
+void LinkedStack::pop() {
+    --n;
     S.removeFront();
-  }
+}
 
-  int main() {
-                                                        // your main code goes here 
-	  return EXIT_SUCCESS;
-  }
+int main() {
+    // your main code goes here
+    return EXIT_SUCCESS;
+}
