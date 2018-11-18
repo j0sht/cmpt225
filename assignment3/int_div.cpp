4ada126d25767aedba7b23c5d057a88ced7342dc
Joshua Tate <jmtate@icloud.com>
2018-11-18 14:37:10 -0800

    Implemented trickleUp, passes initial tests

---
+++ assignment3/int_div.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "2/2 = " << (2/2) << endl;
+  cout << "3/2 = " << (3/2) << endl;
+  cout << "0/2 = " << (0/2) << endl;
+  cout << "1/2 = " << (1/2) << endl;
+  cout << "5/2 = " << (5/2) << endl;
+  cout << "-1/2 = " << (-1/2) << endl;
+  cout << "-2/2 = " << (-2/2) << endl;
+  return 0;
+}
