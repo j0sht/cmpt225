9131fd7ed3f4d53792fbdb185b36c9e97933c35e
Joshua Tate <jmtate@icloud.com>
2018-12-01 17:02:58 -0800

    Implemented answer - passes tests in course-files!

--- assignment4/a4.cpp
+++ assignment4/a4.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 void readFileAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2);
-int answer(priority_queue<int>& p1, priority_queue<int>& p2);
+void answer(priority_queue<int>& p1, priority_queue<int>& p2);
 // debug function
-void printQueues(priority_queue<int>& p1, priority_queue<int>& p2);
+void printQueues(const priority_queue<int>& p1, const priority_queue<int>& p2);
 
 int main() {
   priority_queue<int> p1;
   priority_queue<int> p2;
   cout << "301280241 jmtate Joshua Tate\n";
   readFileAndStoreIn(p1, p2);
-  printQueues(p1, p2);
-  cout << answer(p1,p2) << endl;
+  answer(p1,p2);
+  return 0;
 }
 
 void readFileAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2) {
@@ -33,12 +33,27 @@ void readFileAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2) {
   }
 }
 
-int answer(priority_queue<int>& p1, priority_queue<int>& p2) {
-  // Complete this
-  return 0;
+void answer(priority_queue<int>& p1, priority_queue<int>& p2) {
+  while ((!p1.empty() && !p2.empty()) && (p1.top() <= p2.top())) {
+    if (p1.top() == p2.top()) {
+      int e = p1.top();
+      while ((!p1.empty() && !p2.empty()) && (p1.top() == e || p2.top() == e)) {
+	if (p1.top() == e)
+	  p1.pop();
+	if (p2.top() == e)
+	  p2.pop();
+      }
+    } else { // p1.top() < p2.top()
+      p2.pop();
+    }
+  }
+  if (!p1.empty())
+    cout << p1.top() << endl;
+  else
+    cout << "NONE\n";
 }
 
-void printQueues(priority_queue<int>& p1, priority_queue<int>& p2) {
+void printQueues(const priority_queue<int>& p1, const priority_queue<int>& p2) {
   cout << "p1: ";
   priority_queue<int> tmp = p1;
   while (!tmp.empty()) {
