69f03da8e96f8e919a89a6eab29e7e1643ffec16
Joshua Tate <jmtate@icloud.com>
2018-12-01 20:29:40 -0800

    Changed name of readFileAndStore in to readAndStoreIn; added more comments

--- assignment4/a4.cpp
+++ assignment4/a4.cpp
@@ -4,21 +4,24 @@
 #include <map>
 using namespace std;
 
-void readFileAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2);
+// Reads two lists of integers from stdin and stores them in p1 and p2
+//  while filtering out any duplicate numbers
+void readAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2);
+// Prints the value of the largest element in p1 that is not in p2.
+// Assumes that there are no duplicate entries within either p1 or p2.
 void printLargest(priority_queue<int>& p1, priority_queue<int>& p2);
 
 /* MAIN */
 int main() {
-  priority_queue<int> p1;
-  priority_queue<int> p2;
   cout << "301280241 jmtate Joshua Tate\n";
-  readFileAndStoreIn(p1, p2);
+  priority_queue<int> p1, p2;
+  readAndStoreIn(p1, p2);
   printLargest(p1, p2);
   return 0;
 }
 /* END MAIN */
 
-void readFileAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2) {
+void readAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2) {
   int x;
   bool List2 = false;
   map<int, int> values; // used to ignore repeated values
@@ -41,7 +44,6 @@ void readFileAndStoreIn(priority_queue<int>& p1, priority_queue<int>& p2) {
     cin >> x;
   }
 }
-
 void printLargest(priority_queue<int>& p1, priority_queue<int>& p2) {
   while ((!p1.empty() && !p2.empty()) && (p1.top() <= p2.top())) {
     if (p1.top() == p2.top())
